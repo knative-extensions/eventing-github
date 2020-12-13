ad8da6b5b2b00eb493843811963a50bd048d7a5c
Arghya Sadhu <arghya88@gmail.com>
2020-12-13 09:49:20 +0530

    integrate the gopath hacks
    Signed-off-by: Arghya Sadhu <arghya88@gmail.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -18,22 +18,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-export GO111MODULE=on
+source $(dirname $0)/../vendor/knative.dev/hack/codegen-library.sh
+
 # If we run with -mod=vendor here, then generate-groups.sh looks for vendor files in the wrong place.
 export GOFLAGS=-mod=
 
-if [ -z "${GOPATH:-}" ]; then
-  export GOPATH=$(go env GOPATH)
-fi
+echo "=== Update Codegen for ${MODULE_NAME}"
 
-source $(dirname $0)/../vendor/knative.dev/hack/library.sh
-
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${REPO_ROOT_DIR}; ls -d -1 $(dirname $0)/../vendor/k8s.io/code-generator 2>/dev/null || echo ../../../k8s.io/code-generator)}
-
-KNATIVE_CODEGEN_PKG=${KNATIVE_CODEGEN_PKG:-$(cd ${REPO_ROOT_DIR}; ls -d -1 $(dirname $0)/../vendor/knative.dev/pkg 2>/dev/null || echo ../pkg)}
-
-chmod +x ${CODEGEN_PKG}/generate-groups.sh
-chmod +x ${KNATIVE_CODEGEN_PKG}/hack/generate-knative.sh
+group "Kubernetes Codegen"
 
 # generate the code with:
 # --output-base    because this script should also be able to run inside the vendor dir of
@@ -44,18 +36,23 @@ ${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
 "sources:v1alpha1 bindings:v1alpha1" \
 --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate.go.txt
 
+group "Knative Codegen"
+
 # Knative Injection
 ${KNATIVE_CODEGEN_PKG}/hack/generate-knative.sh "injection" \
 "knative.dev/eventing-github/pkg/client" "knative.dev/eventing-github/pkg/apis" \
 "sources:v1alpha1 bindings:v1alpha1" \
 --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate.go.txt
 
+group "Deepcopy Gen"
 
 # Depends on generate-groups.sh to install bin/deepcopy-gen
 ${GOPATH}/bin/deepcopy-gen \
   -O zz_generated.deepcopy \
   --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate.go.txt \
   -i knative.dev/eventing-github/pkg/apis \
+
+group "Update deps post-codegen"
 
 # Make sure our dependencies are up-to-date
 ${REPO_ROOT_DIR}/hack/update-deps.sh
